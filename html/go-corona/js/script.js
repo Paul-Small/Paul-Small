e6bb446fb9eba5b292ba72f15796d5be80a25ebb
Paul Small <156699496+Paul-Small@users.noreply.github.com>
2024-01-23 13:09:46 +0200

    Delete html directory

--- html/go-corona/js/script.js
+++
@@ -1,9 +0,0 @@
-const menuIcon = document.querySelector('.menu__icon');
-const menuBody = document.querySelector('.menu__body');
-const bodyElement = document.body;
-
-menuIcon.addEventListener('click', (e) => {
-   menuIcon.classList.toggle('active');
-   menuBody.classList.toggle('menu-open');
-   bodyElement.classList.toggle('lock')
-});
